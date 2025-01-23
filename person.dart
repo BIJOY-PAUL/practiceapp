cab7fd3b5f3ee4467b6d8701edc7b0243cd40ca4
BIJOY-PAUL <bijoypal4856@gmail.com>
2025-01-23 21:16:19 +0600

    new program add

--- person.dart
+++ person.dart
@@ -1,19 +1,5 @@
 void main() {
-  Person p = Person("Bijoy Rudra paul", 23,"Raozan");
-  p.displayInfo();
-}
-
-class Person {
-  String? name;
-  int? age;
-
-  String? location;
-
-  Person(this.name, this.age, this.location);
-
-  void displayInfo() {
-    print(name);
-    print(age);
-    print(location);
+  for (int i = 0; i <= 5; i++) {
+    print("hello ${i + 1}");
   }
 }
